7321b535045cd5e1efea61bafda2bdbb7fac2f13
Christian David <c.david@graphscape.de>
2015-10-20 14:39:06 +0200

    tools: enable opslog for dev environment (for realtime updates between the apps)

--- tools/start-all-apps.sh
+++ tools/start-all-apps.sh
@@ -3,16 +3,16 @@
 # script runs from "/tools" folder
 if [ -d ../admin ]; then
     (cd ../admin && meteor --port 3002) &
-    (cd ../merchants && MONGO_URL='mongodb://localhost:3003/meteor' meteor --port 3001) &
-    (cd ../customers && MONGO_URL='mongodb://localhost:3003/meteor' meteor --port 3000) &
+    (cd ../merchants && MONGO_OPLOG_URL='mongodb://localhost:3003/local' MONGO_URL='mongodb://localhost:3003/meteor' meteor --port 3001) &
+    (cd ../customers && MONGO_OPLOG_URL='mongodb://localhost:3003/local' MONGO_URL='mongodb://localhost:3003/meteor' meteor --port 3000) &
     echo "Devtools at: http://localhost:3002/devtools"
 fi
 
 # script runs from "/" folder
 if [ -d admin ]; then
     (cd admin && meteor --port 3002) &
-    (cd merchants && MONGO_URL='mongodb://localhost:3003/meteor' meteor --port 3001) &
-    (cd customers && MONGO_URL='mongodb://localhost:3003/meteor' meteor --port 3000) &
+    (cd merchants && MONGO_OPLOG_URL='mongodb://localhost:3003/local' MONGO_URL='mongodb://localhost:3003/meteor' meteor --port 3001) &
+    (cd customers && MONGO_OPLOG_URL='mongodb://localhost:3003/local' MONGO_URL='mongodb://localhost:3003/meteor' meteor --port 3000) &
     echo "Devtools at: http://localhost:3002/devtools"
 fi
 
