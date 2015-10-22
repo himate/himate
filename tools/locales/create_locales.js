a5606cabafa8a21f6a94e3472d200c0df7435ea8
DelodiTR <tr@delodi.net>
2015-10-22 14:30:06 +0200

    created csv parser for locales

---
+++ tools/locales/create_locales.js
@@ -0,0 +1,44 @@
+var fs = require('fs');
+var parse = require('csv-parse');
+
+var inputFile = 'locale.csv';
+
+var targets = [
+    '../../admin/lib/locale/',
+    '../../merchants/lib/locale/',
+    '../../customers/lib/locale/',
+    './'
+]
+
+var parser = parse({delimiter: ','}, function (err, data) {
+    //console.log(data);
+    var locales = {};
+    var columns = [];
+    data.forEach(function (row, index) {
+        if (index == 0) { // handle header
+            row.forEach(function (value, index) {
+                columns.push(value);
+                if (index > 0) {
+                    locales[value] = {};
+                }
+            });
+        } else {
+            row.forEach(function (value, index) {
+                if (index > 0) {
+                    locales[columns[index]][row[0]] = value;
+                }
+            });
+        }
+
+    });
+    columns.forEach(function (value, index) {
+        if (index > 0) {
+            targets.forEach(function (target) {
+                var filename = value + '.i18n.json';
+                console.log('copying ' + filename +' to ' + target);
+                fs.writeFile(target + filename, JSON.stringify(locales[value], null, 4));
+            });
+        }
+    });
+});
+fs.createReadStream(inputFile).pipe(parser);
