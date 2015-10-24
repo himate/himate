b30dd231b97ada284ed935d7ba82b2b53a5cef1e
DelodiTR <tr@delodi.net>
2015-10-24 14:34:02 +0200

    add registration for merchants

--- merchants/lib/accounts_templates/settings.js
+++ merchants/lib/accounts_templates/settings.js
@@ -5,20 +5,20 @@ AccountsTemplates.configure({
 
     // Appearance
     showAddRemoveServices: false,
-    showForgotPasswordLink: false,
+    showForgotPasswordLink: true,
     showLabels: true,
     showPlaceholders: true,
 
     // Behaviours
     confirmPassword: true,
     enablePasswordChange: false,
-    forbidClientAccountCreation: true,
+    forbidClientAccountCreation: false,
     sendVerificationEmail: false,
     homeRoutePath: '/',
     redirectTimeout: 200,
 
     // Hooks
-    onLogoutHook: function() {
+    onLogoutHook: function () {
         // redirect to start page after logout
         Router.go('pages_startpage');
     }
