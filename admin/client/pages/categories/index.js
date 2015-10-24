2e4f2a3780cfe1720afb0629f4fed52496a7ac3d
Raed Marji <raed@jogeeks.com>
2015-10-24 14:58:21 +0200

    add events categories

--- admin/client/pages/categories/index.js
+++ admin/client/pages/categories/index.js
@@ -12,3 +12,15 @@ Template.pages_categories.helpers({
         return Categories.find();
     }
 });
+
+Template.pages_categories.events({
+
+    "click #add_cat" :function (event){
+        console.log("a link was clicked");
+    },
+
+    "click .remove-category" :function (event){
+        //console.log("remove cat was clicked");
+        Meteor.call("categories_remove",this._id);
+    }
+});
