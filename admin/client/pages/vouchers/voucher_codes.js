0f9caaca10d104d2d6bd96c649a1c31653edc9f8
DelodiTR <tr@delodi.net>
2015-10-25 10:05:02 +0100

    fix voucher codes listing

--- admin/client/pages/vouchers/voucher_codes.js
+++ admin/client/pages/vouchers/voucher_codes.js
@@ -12,5 +12,12 @@ Template.pages_voucher_codes.helpers({
       console.log("Router.current().params._id", Router.current().params._id);
       console.log("VoucherCodes.find()", VoucherCodes.find().fetch());
         return VoucherCodes.find();
+    },
+
+});
+
+Template.pages_voucher_codes_code.helpers({
+    user: function(){
+        return Meteor.users.findOne(this.userId).username;
     }
 });
