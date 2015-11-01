c6ed9895a8b4fe07fe2f51c2abd5880208ee8bd4
Christian David <c.david@graphscape.de>
2015-11-01 07:43:24 +0100

    refactoring: introducing "apps" root folder

--- tools/start-all-apps.sh
+++ tools/start-all-apps.sh
@@ -1,24 +1,20 @@
 #!/usr/bin/env bash
 
-
-
 # script runs from "/tools" folder
-if [ -d ../admin ]; then
+if [ -d ../apps/admin ]; then
     export PACKAGE_DIRS=`pwd`/../packages
-	#export METEOR_SETTINGS="$(cat ../settings.json)"
-    (cd ../admin && meteor --port 3002 --settings `pwd`/../settings.json) &
-    (cd ../merchants && MONGO_OPLOG_URL='mongodb://localhost:3003/local' MONGO_URL='mongodb://localhost:3003/meteor' meteor --port 3001 --settings `pwd`/../settings.json) &
-    (cd ../customers && MONGO_OPLOG_URL='mongodb://localhost:3003/local' MONGO_URL='mongodb://localhost:3003/meteor' meteor --port 3000 --settings `pwd`/../settings.json) &
+    (cd ../apps/admin && meteor --port 3002 --settings `pwd`/../settings.json) &
+    (cd ../apps/merchants && MONGO_OPLOG_URL='mongodb://localhost:3003/local' MONGO_URL='mongodb://localhost:3003/meteor' meteor --port 3001 --settings `pwd`/../settings.json) &
+    (cd ../apps/customers && MONGO_OPLOG_URL='mongodb://localhost:3003/local' MONGO_URL='mongodb://localhost:3003/meteor' meteor --port 3000 --settings `pwd`/../settings.json) &
     echo "Devtools at: http://localhost:3002/devtools"
 fi
 
 # script runs from "/" folder
-if [ -d admin ]; then
+if [ -d apps/admin ]; then
     export PACKAGE_DIRS=`pwd`/packages
-	#export METEOR_SETTINGS="$(cat settings.json)"
-    (cd admin && meteor --port 3002 --settings `pwd`/../settings.json) &
-    (cd merchants && MONGO_OPLOG_URL='mongodb://localhost:3003/local' MONGO_URL='mongodb://localhost:3003/meteor' meteor --port 3001 --settings `pwd`/../settings.json) &
-    (cd customers && MONGO_OPLOG_URL='mongodb://localhost:3003/local' MONGO_URL='mongodb://localhost:3003/meteor' meteor --port 3000 --settings `pwd`/../settings.json) &
+    (cd apps/admin && meteor --port 3002 --settings `pwd`/../settings.json) &
+    (cd apps/merchants && MONGO_OPLOG_URL='mongodb://localhost:3003/local' MONGO_URL='mongodb://localhost:3003/meteor' meteor --port 3001 --settings `pwd`/../settings.json) &
+    (cd apps/customers && MONGO_OPLOG_URL='mongodb://localhost:3003/local' MONGO_URL='mongodb://localhost:3003/meteor' meteor --port 3000 --settings `pwd`/../settings.json) &
     echo "Devtools at: http://localhost:3002/devtools"
 fi
 
