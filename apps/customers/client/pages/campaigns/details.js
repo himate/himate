5b398d0f31030ccf11f1eb1425f6ca89d0a235ae
DelodiTR <tr@delodi.net>
2015-11-21 14:13:38 +0100

    #27 show reservation login modal if user is not registered

--- apps/customers/client/pages/campaigns/details.js
+++ apps/customers/client/pages/campaigns/details.js
@@ -8,7 +8,7 @@ Template.pages_campaigns_details.helpers({
      * return data for current voucher
      * @reactive
      */
-    item: function() {
+    item: function () {
         var result = Waslchiraa.Collections.Campaigns.findOne({
             _id: Router.current().params._id
         });
@@ -26,18 +26,17 @@ Template.pages_campaigns_details.events({
      * @param {Object} event
      * @param {Object} template
      */
-    'click .js-reserve-voucher': function(event, template) {
-        var $modal = $('.modal.confirmation');
-        $modal
+    'click .js-reserve-voucher': function (event, template) {
+        var $modalConfirmation = $('.modal.confirmation')
             .modal({
-                closable  : true,
-                onDeny    : function(){
-                    $modal.modal('hide');
+                closable: true,
+                onDeny: function () {
+                    $modalConfirmation.modal('hide');
                 },
-                onApprove : function() {
+                onApprove: function () {
                     var campaign = Waslchiraa.Collections.Campaigns.findOne(Router.current().params._id);
                     if (campaign) {
-                        var vouchercode = Meteor.call('vouchers_reserve', campaign._id.toString(), function(err, data) {
+                        var vouchercode = Meteor.call('vouchers_reserve', campaign._id.toString(), function (err, data) {
                             if (err) {
                                 Waslchiraa.Helpers.errorMessage(err.message);
                             }
@@ -46,10 +45,29 @@ Template.pages_campaigns_details.events({
                             }
                         });
                     }
-                    $modal.modal('hide');
+                    $modalConfirmation.modal('hide');
                 }
-            })
-            .modal('show')
+            });
+        var $modalLogin = $('.modal.login')
+            .modal({
+                closable: true,
+                onDeny: function () {
+                    $modalLogin.modal('hide');
+                    $('.ui.sidebar').sidebar('toggle');
+                    $('.at-signup').trigger('click');
+                },
+                onApprove: function () {
+                    $modalLogin.modal('hide');
+                    $('.ui.sidebar').sidebar('toggle');
+                    $('.at-signin').trigger('click');
+                }
+            });
+        if (Meteor.user()) {
+            $modalConfirmation.modal('show');
+        } else {
+
+            $modalLogin.modal('show');
+        }
     },
 });
 
