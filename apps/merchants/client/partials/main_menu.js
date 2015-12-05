35cd7b9922477947598121bfee451ce10b07982b
Sana Moussa <sm@delodi.net>
2015-12-05 15:15:49 +0100

    sidebar

--- apps/merchants/client/partials/main_menu.js
+++ apps/merchants/client/partials/main_menu.js
@@ -17,7 +17,15 @@ Template.partials_main_menu.events({
      * @param {Object} event
      */
     'click .js-menu': function(event) {
-        $('#partials-sidebar').sidebar('setting', 'transition', 'push').sidebar('toggle');
+        $('.ui.sidebar').sidebar('toggle');
+        return Waslchiraa.Helpers.cancel(event);
+    },
+    /**
+     * open/close the sidebar for mobile users
+     * @param {Object} event
+     */
+    'touchstart .js-menu': function(event) {
+        $('.ui.sidebar').sidebar('toggle');
         return Waslchiraa.Helpers.cancel(event);
     },
 
