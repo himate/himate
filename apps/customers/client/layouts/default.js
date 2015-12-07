e164ef37d852e709d86ae5074924c77d5012a8a5
Sana Moussa <sm@delodi.net>
2015-12-07 13:03:34 +0100

    add touchstart event

--- apps/customers/client/layouts/default.js
+++ apps/customers/client/layouts/default.js
@@ -5,5 +5,12 @@ Template.layouts_default.events({
         $('.ui.sidebar').sidebar('toggle');
         $('#partials-main-menu').removeClass('active');
         $('#partials-languages').removeClass('active');
+    },
+    /**
+     */
+    'touchstart .pusher': function() {
+        $('.ui.sidebar').sidebar('toggle');
+        $('#partials-main-menu').removeClass('active');
+        $('#partials-languages').removeClass('active');
     }
 });
--- apps/merchants/client/layouts/default.js
+++ apps/merchants/client/layouts/default.js
@@ -4,5 +4,11 @@ Template.layouts_default.events({
     'click .pusher': function() {
         $('.ui.sidebar').sidebar('toggle');
         $('#partials-main-menu').removeClass('active');
+    },
+    /**
+     */
+    'touchstart .pusher': function() {
+        $('.ui.sidebar').sidebar('toggle');
+        $('#partials-main-menu').removeClass('active');
     }
 });
