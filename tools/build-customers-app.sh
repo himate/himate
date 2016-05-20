8b194c690976f33708076991a0c512c7e8712f53
DelodiTR <tr@delodi.net>
2016-05-20 16:33:48 +0200

    update buildscript for android

--- tools/build-customers-app.sh
+++ tools/build-customers-app.sh
@@ -8,9 +8,10 @@ if [ -d apps/customers ]; then
     (cd apps/customers && meteor build ../../build/customers/ --server=https://app.himate.org)
 
     echo "Signing..."
-    (cd build/customers/android/ && jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 release-unsigned.apk himate-app)
+    (cd build/customers/android/ && jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore himate.keystore  release-unsigned.apk himate-app)
 
     echo "Zipalign..."
-    (cd build/customers/android/ && rm production.apk && $ANDROID_HOME/build-tools/23.0.1/zipalign 4 release-unsigned.apk production.apk)
+     (cd build/customers/android/ && rm production.apk)
+    (cd build/customers/android/ &&  $ANDROID_HOME/build-tools/23.0.1/zipalign 4 release-unsigned.apk production.apk)
 fi
 
