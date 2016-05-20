b147b651bd3fa379657f832f484e3da0fbc36526
DelodiTR <tr@delodi.net>
2016-05-20 16:54:11 +0200

    add comment for precondition to build

--- tools/build-customers-app.sh
+++ tools/build-customers-app.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
 # script should run from "/" folder
+# before you are able to build you have to create a keystore. go to build/customers/android and run
+# keytool -genkey -v -keystore himate.keystore -alias himate-app -keyalg RSA -keysize 2048 -validity 10000
+
 # himateTEST
 if [ -d apps/customers ]; then
     export PACKAGE_DIRS=`pwd`/packages
