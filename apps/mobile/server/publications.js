b8c90f93fa53f8a401527b225d161a1ef7a212ae
DelodiTR <tr@delodi.net>
2016-09-02 14:51:38 +0200

    refactor publications for client and mobile

--- apps/mobile/server/publications.js
+++ apps/mobile/server/publications.js
@@ -1,118 +1,19 @@
-/**
- * publish all campaigns
- */
+
 Meteor.publish('campaigns', function () {
-
-    // collect data
-    var today = moment().endOf('day').toDate();
-    var campaigns = HiMate.Collections.Campaigns.find({
-        $or: [{
-            end: null
-        }, {
-            $and: [{
-                approved: true
-            },{
-                published: {
-                    $lte: today
-                }
-            }, {
-                published: {
-                    $exists: true
-                }
-            }, {
-                published: {
-                    $ne: null
-                }
-            }]
-        }]
-    });
-
-    var merchantIds = campaigns.map(function (v) {
-        return v.userId;
-    });
-
-   /* var categoryIds = campaigns.map(function (v) {
-        return v.categoryId;
-    });
-
-    var categories = HiMate.Collections.Categories.find({
-        _id: {
-            $in: categoryIds
-        }
-
-    });
-*/
-    var merchants = Meteor.users.find({
-        _id: {
-            $in: merchantIds
-        }
-    }, {
-        fields: {
-            "profile.company": 1,
-            "profile.firstName": 1,
-            "profile.lastName": 1,
-            "profile.street": 1,
-            "profile.number": 1,
-            "profile.zipcode": 1,
-            "profile.city": 1,
-            "profile.country": 1
-        }
-    });
-
-    // send collections
-    return [campaigns, merchants];
+    return HiMate.Publications.customers.campaigns.call(this);
 });
 
 Meteor.publish('categories', function (categoryIds) {
-    return HiMate.Collections.Categories.find({_id: {$in: categoryIds}});
+    return HiMate.Publications.customers.categories.call(this,categoryIds);
 });
 
 
-/**
- *
- */
 Meteor.publish('images', function (campaignIds) {
 
-    check(campaignIds, Match.Optional(Array));
-
-    var filter = {};
-    if (campaignIds) {
-        filter._id = {
-            $in: campaignIds
-        };
-    }
-
-    var imageIds = HiMate.Collections.Campaigns.find(filter).map(function (v) {
-        return v.imageId;
-    });
-
-    //
-    return HiMate.Collections.Images.find({
-        _id: {
-            $in: imageIds
-        }
-    });
+    return HiMate.Publications.customers.images.call(this.campaignIds);
 });
 
-/**
- *
- */
 Meteor.publish('vouchers', function (campaignIds) {
+    return HiMate.Publications.customers.vouchers.call(this,campaignIds);
 
-    // security checks
-    if (!Roles.userIsInRole(this.userId, 'customer')) {
-        return this.ready();
-    }
-
-    check(campaignIds, Match.Optional(Array));
-
-    var filter = {};
-    filter.userId = this.userId;
-    if (campaignIds) {
-        filter.campaignId = {
-            $in: campaignIds
-        };
-    }
-
-    return HiMate.Collections.Vouchers.find(filter);
 });
