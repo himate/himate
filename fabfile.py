0384177f2429173e8b940045ebe7093dc9c96ba4
Karl Fischer <karl.fischer@project-a.com>
2016-09-25 12:07:54 +0200

    Fabfile

--- fabfile.py
+++ fabfile.py
@@ -1,5 +1,8 @@
-from fabric.api import (sudo, local)
-from fabric.context_managers import lcd, shell_env
+from fabric.api import (sudo, put, local, run)
+from fabric.context_managers import (lcd, shell_env)
+
+
+BUILD_DIR = 'cur_build'
 
 
 def _docker_login(user, password, domain):
@@ -12,6 +15,18 @@ def _docker_logout(domain):
     sudo('docker logout %s' % (domain))
 
 
-def build_meteor_branch(branch_name, app_type, release='1.3.3'):
+def build_branch(branch_name, app_type, release='1.3.3'):
     with lcd('apps/%s' % (app_type)), shell_env(PACKAGE_DIRS='../../packages'):
-        local('meteor --release %s build ../../cur_build' % (release))
+        local('meteor --release %s build ../../%s' % (release, BUILD_DIR))
+
+
+def build_image(branch_name, app_type, hub, repo, hub_user, hub_pass):
+    put('Dockerfile', 'Dockerfile')
+    put('%s/%s.tar.gz' % (BUILD_DIR, app_type), '%s.tar.gz' % (app_type))
+    run('tar -xf %s.tar.gz -C %s' % (app_type, app_type))
+    sudo('docker build -t %s/%s:%s-SNAPSHOT .' % (hub, repo, branch_name))
+    sudo('docker login --username="%s" --password="%s" %s' % (hub_user,
+                                                              hub_pass,
+                                                              hub))
+    sudo('docker push %s/%s:%s-SNAPSHOT ' % (hub, repo, branch_name))
+    sudo('docker logout %s' % (hub))
