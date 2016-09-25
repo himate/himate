965d444e7a08d2bec644c72260ad91b682811dfd
Karl Fischer <karl.fischer@project-a.com>
2016-09-25 11:11:48 +0200

    Further fabfile fix

--- fabfile.py
+++ fabfile.py
@@ -1,5 +1,5 @@
 from fabric.api import (sudo, local)
-from fabric.context_managers import lcd
+from fabric.context_managers import lcd, shell_env
 
 
 def _docker_login(user, password, domain):
@@ -13,5 +13,5 @@ def _docker_logout(domain):
 
 
 def build_meteor_branch(branch_name, app_type, release='1.3.3'):
-    with lcd('apps/%s' % (app_type)):
+    with lcd('apps/%s' % (app_type)), shell_env(PACKAGE_DIRS='packages'):
         local('meteor --release %s build build' % (release))
