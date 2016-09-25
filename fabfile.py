2384819885f76a43438725d4bacbc2f69d2c97b4
Karl Fischer <karl.fischer@project-a.com>
2016-09-25 11:15:38 +0200

    Fabfile

--- fabfile.py
+++ fabfile.py
@@ -13,5 +13,6 @@ def _docker_logout(domain):
 
 
 def build_meteor_branch(branch_name, app_type, release='1.3.3'):
-    with lcd('apps/%s' % (app_type)), shell_env(PACKAGE_DIRS='packages'):
-        local('meteor --release %s build build' % (release))
+    with shell_env(PACKAGE_DIRS='packages'):
+        with lcd('apps/%s' % (app_type)):
+            local('meteor --release %s build build' % (release))
